873e1dad7c7518075522d317e79b08e840295941
Nathan Schmitz <nathanschmitz@unomaha.edu>
2022-08-30 01:12:11 +0000

    initial commit

--- hello.c
+++ hello.c
@@ -5,6 +5,8 @@ int main(int argc, char **argv) {
 
     printf("Danny!\n");
     printf("Electrical Engineering\n");
+    printf("Nathan\n");
+    printf("Exploratory Studies\n");
 
     return 0;
 }
